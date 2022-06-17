9e96851a97d110ddf91a5fa911a9882ae5b3ef53
kintrix <60898798+kintrix007@users.noreply.github.com>
2022-06-17 14:09:21 +0200

    Should be ran with sudo

--- install.sh
+++ install.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Update xbps
-sudo xbps-install -Suy || sudo xbps-install -uy xbps
-sudo xbps-install -uy
+xbps-install -Suy || sudo xbps-install -uy xbps
+xbps-install -uy
 
 # Add build-in repos
-sudo xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
 # Add and enable services
 services=`echo socklog-unix nanoklogd snooze-{hourly,daily,weekly,monthly} isc-ntpd tlp dbus elogind`
-sudo xbps-install -y socklog-void snooze ntp tlp dbus elogind
+xbps-install -y socklog-void snooze ntp tlp dbus elogind
 
 for serv in $services; do
-	[[ -f /var/service/$serv ]] || sudo ln -s /etc/sv/$serv /var/service/$serv
-	sudo sv up $serv
+	[[ -f /var/service/$serv ]] || ln -s /etc/sv/$serv /var/service/$serv
+	sv up $serv
 done
 
 # Set up weekly fstrim
-[[ -d /etc/cron.weekly ]] || sudo mkdir /etc/cron.weekly/
-cat << EOF | sudo tee /etc/cron.weekly/fstrim > /dev/null
+[[ -d /etc/cron.weekly ]] || mkdir /etc/cron.weekly/
+cat << EOF > /etc/cron.weekly/fstrim
 #!/bin/sh
 
 fstrim /
 EOF
-sudo chmod +x /etc/cron.weekly/fstrim
+chmod +x /etc/cron.weekly/fstrim
 
 # Set up flatpak
-sudo xbps-install -y flatpak
+xbps-install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Set up graphical interface
-sudo xbps-install -y xorg xinit ghc xmonad xmonad-contrib dmenu
+xbps-install -y xorg xinit ghc xmonad xmonad-contrib dmenu
 
 cat << EOF > ~/.xinitrc
 exec xmonad
@@ -39,4 +39,4 @@ EOF
 
 # Install user packages
 PACKAGES=`sed s/#.*// ./packages`
-sudo xbps-install -y $PACKAGES
+xbps-install -y $PACKAGES
