612f2c3df1a297c19054ad0af3f399419cac0287
kintrix <boldizsar02@gmail.com>
2022-06-17 12:20:54 +0200

    Added lines making sure xbps is updated

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Update xbps
+sudo xbps-install -Suy || sudo xbps-install -y xbps
+sudo xbps-install -uy
+
 # Add build-in repos
-sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+sudo xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
 # Add and enable services
 services=`echo socklog-unix nanoklogd snooze-{hourly,daily,weekly,monthly} isc-ntpd tlp dbus elogind`
-sudo xbps-install -Sy socklog-void snooze ntp tlp dbus elogind
+sudo xbps-install -y socklog-void snooze ntp tlp dbus elogind
 
 for serv in $services; do
 	sudo ln -s /etc/sv/$serv /var/service/
