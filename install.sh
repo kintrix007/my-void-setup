a68bca7394d6a088e1b527b6ec052ba0eb37fa3e
kintrix <boldizsar02@gmail.com>
2022-06-18 15:03:39 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Update xbps
-xbps-install -Suy || sudo xbps-install -uy xbps
 xbps-install -Suy
+if [[ $? != 0 ]]; then
+	sudo xbps-install -uy xbps
+	xbps-install -Suy
+fi
 
 # Add build-in repos
 xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
@@ -16,6 +19,43 @@ for serv in $services; do
 	[[ -f /var/service/$serv ]] || ln -s /etc/sv/$serv /var/service/$serv
 	sv up $serv
 done
+
+# Set up bash environment
+xbps-install bash_completion
+
+cat << EOF > /etc/bash/bashrc.d/bash_completion.sh
+# bash_completion.sh
+
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+  . /usr/share/bash-completion/bash_completion
+fi
+EOF
+
+cat << EOF > ~/.bash_profile
+# .bash_profile
+
+# Get the aliases and functions
+[ -f \$HOME/.bashrc ] && . \$HOME/.bashrc
+
+# Add local bin directory to PATH
+export PATH=\$PATH:\$HOME/.local/bin
+EOF
+
+cat << EOF > ~/.bashrc
+# .bashrc
+
+# if not running interactively, don't do anything
+[[ \$- != *i* ]] && return
+
+# Load user aliases
+[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases
+
+# Set defaul editor
+export EDITOR=vim
+
+# Set console prompt
+PS1='\[\e[1m\]'['\[\e[[92m\]'\u'\[\e[0m\]'@'\[\e[1;92m\]'\h '\[\e[94m\]'\W'\[\e[1m\]']\$ '\[\e[0m\]'
+EOF
 
 # Set up weekly fstrim
 [[ -d /etc/cron.weekly ]] || mkdir /etc/cron.weekly/
@@ -31,7 +71,7 @@ xbps-install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Set up graphical interface
-xbps-install -y xorg xinit dmenu ghc
+xbps-install -y xorg xinit dmenu stack
 
 cat << EOF > ~/.xinitrc
 exec xmonad
@@ -46,5 +86,4 @@ packages=`sed s/#.*// ./flatpak-packages`
 for pack in $packages; do
 	flatpak install flathub $pack -y
 done
-
 
