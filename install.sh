df028b2d897038d6008f91c439a0b469b90d8001
kintrix <60898798+kintrix007@users.noreply.github.com>
2022-06-19 18:17:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ sudo ./root-install.sh
 # Set up bash profile
 cat << EOF > ~/.bash_profile
 # .bash_profile
+
 # Get the aliases and functions
 [ -f \$HOME/.bashrc ] && . \$HOME/.bashrc
+
 # Add local bin directory to PATH
 export PATH=\$PATH:\$HOME/.local/bin
 EOF
@@ -15,12 +17,16 @@ EOF
 # Set up bashrc
 cat << EOF > ~/.bashrc
 # .bashrc
+
 # if not running interactively, don't do anything
 [[ \$- != *i* ]] && return
+
 # Load user aliases
 [[ -f ~/.bash_aliases ]] && . ~/.bash_aliases
+
 # Set defaul editor
 export EDITOR=vim
+
 # Set console prompt
 PS1='\[\e[1m\][\[\e[92m\]\u\[\e[0m\]@\[\e[1;92m\]\h \[\e[94m\]\W\[\e[0;1m\]]\$ \[\e[0m\]'
 EOF
